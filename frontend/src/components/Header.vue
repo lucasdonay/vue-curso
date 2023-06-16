8c1125692a0f103b9faf3b7a8c0c3985cd7601a1
Donay <donay@ingressolive.com>
2023-06-15 23:06:52 -0300

    continuando os estudos

---
+++ frontend/src/components/Header.vue
@@ -0,0 +1,10 @@
+<template></template>
+
+
+<script>
+export default {
+  name: 'header'
+}
+</script>
+
+<style scoped></style>
--- frontend/src/views/DonayView.vue
+++ frontend/src/views/DonayView.vue
@@ -1,15 +1,12 @@
 <template>
-  <div class="about">
-    <h1>DonayView</h1>
-  </div>
+  <h1 id="app-products">Products</h1>
 </template>
 
 <style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
+#app-products {
+  color: red;
 }
 </style>
+<script>
+alert('ola donay')
+</script>
